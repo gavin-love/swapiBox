9511bb56049e0264264fe0092ae3c2cabea6e53f
gavin-love <1co.loco@oprotonmail.ch>
2018-07-07 13:56:59 -0600

    Refactor cardContainer to implement favorites

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,38 +1,38 @@
 import React from "react";
 
-const Card = props => {
-  if (props.homeworld) {
+const Card = ({ info, handleFavorites }) => {
+  if (info.homeworld) {
     return (
       <div>
-        <h1>{props.name}</h1>
-        <h1>{props.homeworld}</h1>
-        <h1>{props.species}</h1>
-        <h1>{props.population}</h1>
-        <button onClick={() => props.handleFavorites(props)}>favorite</button>
+        <h1>{info.name}</h1>
+        <h1>{info.homeworld}</h1>
+        <h1>{info.species}</h1>
+        <h1>{info.population}</h1>
+        <button onClick={() => handleFavorites(info)}>favorite</button>
       </div>
     );
   }
 
-  if (props.terrain) {
+  if (info.terrain) {
     return (
       <div>
-        <h1>{props.name}</h1>
-        <h1>{props.terrain}</h1>
-        <h1>{props.population}</h1>
-        <h1>{props.climate}</h1>
-        <h1>{props.residents.map(resident => resident.name)}</h1>
-        <button onClick={() => props.handleFavorites(props)}>favorite</button>
+        <h1>{info.name}</h1>
+        <h1>{info.terrain}</h1>
+        <h1>{info.population}</h1>
+        <h1>{info.climate}</h1>
+        <h1>{info.residents.map(resident => resident.name)}</h1>
+        <button onClick={() => handleFavorites(info)}>favorite</button>
       </div>
     );
   }
-  if (props.passengers) {
+  if (info.passengers) {
     return (
       <div>
-        <h1>{props.name}</h1>
-        <h1>{props.model}</h1>
-        <h1>{props.class}</h1>
-        <h1>{props.passengers}</h1>
-        <button onClick={() => props.handleFavorites(props)}>favorite</button>
+        <h1>{info.name}</h1>
+        <h1>{info.model}</h1>
+        <h1>{info.class}</h1>
+        <h1>{info.passengers}</h1>
+        <button onClick={() => handleFavorites(info)}>favorite</button>
       </div>
     );
   }
--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,60 +1,11 @@
 import React from "react";
 import Card from "../Card";
 
-const CardContainer = props => {
-  if (props.data.selected === "people") {
-    const people = props.data.people.map((person, index) => {
-      return (
-        <Card
-          name={person.name}
-          homeworld={person.homeworld}
-          species={person.species}
-          population={person.population}
-          favorite={person.favorite}
-          key={index}
-          handleFavorites={props.handleFavorites}
-        />
-      );
-    });
-    return <div className="card-container">{people}</div>;
-  }
-
-  if (props.data.selected === "planets") {
-    const planets = props.data.planets.map((planet, index) => {
-      return (
-        <Card
-          name={planet.name}
-          terrain={planet.terrain}
-          population={planet.population}
-          climate={planet.climate}
-          residents={planet.residents}
-          favorite={planet.favorite}
-          key={index}
-          handleFavorites={props.handleFavorites}
-        />
-      );
-    });
-    return <div className="card-container">{planets}</div>;
-  }
-
-  if (props.data.selected === "vehicles") {
-    const vehicles = props.data.vehicles.map((vehicle, index) => {
-      return (
-        <Card
-          name={vehicle.name}
-          model={vehicle.model}
-          class={vehicle.clAss}
-          passengers={vehicle.passengers}
-          favorite={vehicle.favorite}
-          key={index}
-          handleFavorites={props.handleFavorites}
-        />
-      );
-    });
-    return <div className="card-container">{vehicles}</div>;
-  }
-
-  return <div />;
+const CardContainer = ({ cards, handleFavorites }) => {
+  const card = cards.map((item, index) => {
+    return <Card info={item} key={index} handleFavorites={handleFavorites} />;
+  });
+  return <div className="card-container">{card}</div>;
 };
 
 export default CardContainer;
