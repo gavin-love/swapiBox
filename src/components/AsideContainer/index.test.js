7b183c23bcacbb1778d214c8d4b1f2a55d59045a
gavin-love <1co.loco@oprotonmail.ch>
2018-07-07 17:05:45 -0600

    Write Card, CardContainer, AsideContainer tests

---
+++ src/components/AsideContainer/index.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { AsideContainer } from "./index.js";
+import { shallow } from "enzyme";
+
+describe("AsideContainer", () => {
+  let wrapper;
+  let mockData;
+
+  it("matches the snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should return opening scroll when pass film", () => {
+    mockData = {
+      title: "A NEW HOPE",
+      releaseDate: "1977-05-25",
+      openingCrawl: "IT'S A WAR"
+    };
+
+    wrapper = <AsideContainer film={mockData} />;
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
